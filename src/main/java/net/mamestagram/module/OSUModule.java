98fbcdf544afe25cd00d4f9bd0b695ecf44efe86
mames1dev <kamakuraowner@outlook.jp>
2023-03-22 21:55:43 +0900

    例外処理を修正

--- src/main/java/net/mamestagram/module/OSUModule.java
+++ src/main/java/net/mamestagram/module/OSUModule.java
@@ -38,15 +38,10 @@ public class OSUModule {
     public static String getModsName(int n) {
 
         ArrayList<String> mod = new ArrayList<>();
-        final String[] mods = {"NF", "EZ", "TS", "HD", "HR", "SD", "DT", "RX", "HT", "NC", "FL", "", "SO", "AP", "PF"};
+        final String[] mods = {"NF", "EZ", "TS", "HD", "HR", "SD", "DT", "RX", "HT", "NC", "FL", "", "SO", "AP", "PF", "4K", "5K", "6K", "7K", "8K", "FD", "RD", "CM", "TG", "9K", "KC", "1K", "3K", "2K", "V2", "MR"};
         StringBuilder rMods = new StringBuilder();
 
-        if (n >= 536870912) {
-            mod.add("V2");
-            n -= 536870912;
-        }
-
-        for (int i = 14; i >= 0; i--) {
+        for (int i = 30; i >= 0; i--) {
             if (i != 2 && i != 11 && n >= Math.pow(2, i)) {
                 switch (i) {
                     case 14 -> n -= Math.pow(2, 5);
