d0a1557c687e5c2ef5919a108b9168cad012bc63
mames1dev <kamakuraowner@outlook.jp>
2023-04-05 16:45:30 +0900

    apikey含まれたので再コミット

--- src/main/java/net/mamestagram/module/TranslateModule.java
+++ src/main/java/net/mamestagram/module/TranslateModule.java
@@ -11,13 +11,14 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static net.mamestagram.Main.*;
+
 public class TranslateModule {
 
     public static String getTranslateSentence(String text, String targetLang) throws InterruptedException, IOException {
 
         //定数のセット
         final String url = "https://api-free.deepl.com/v2/translate";
-        final String authKey = "5bd409a9-8923-e8ff-5730-c906c97f1808:fx";
 
         // UTF-8にエンコード
         String encodedText = URLEncoder.encode(text, "UTF-8");
@@ -31,7 +32,7 @@ public class TranslateModule {
         // DeeplのAPIを参考にリクエストを追加
         HttpRequest httpRequest = HttpRequest.newBuilder()
                 .uri(URI.create(url))
-                .header("Authorization", "DeepL-Auth-Key " + authKey)
+                .header("Authorization", "DeepL-Auth-Key " + deeplAPIKEY)
                 .header("User-Agent", "YourApp/1.2.3")
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .POST(HttpRequest.BodyPublishers.ofString(requestBody))
