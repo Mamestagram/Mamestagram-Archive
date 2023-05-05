9fea27a9db0ab5181f4c2b118ebe83d87e687139
mames1dev <kamakuraowner@outlook.jp>
2023-05-06 00:00:05 +0900

    コードの最適化

--- src/main/java/net/mamestagram/server/CurrentLoginActivity.java
+++ src/main/java/net/mamestagram/server/CurrentLoginActivity.java
@@ -16,30 +16,16 @@ public class CurrentLoginActivity {
 
         PreparedStatement ps;
         ResultSet result;
-        List<Integer> userID = new ArrayList<>();
-        int count = 0, activeCount = 0;
+        int count = 0;
 
-        ps = connection.prepareStatement("select id from users");
+        ps = connection.prepareStatement("select latest_activity from users");
         result = ps.executeQuery();
         while (result.next()) {
-            userID.add(result.getInt("id"));
-            count++;
-        }
-
-        if(count >= 1) {
-            for (int i : userID) {
-                long timestamp = System.currentTimeMillis() / 1000;
-
-                ps = connection.prepareStatement("select latest_activity from users where id = ?");
-                ps.setInt(1, i);
-                result = ps.executeQuery();
-                if(result.next()) {
-                    if(timestamp - 600L < result.getLong("latest_activity")) {
-                        activeCount++;
-                    }
-                }
+            if((System.currentTimeMillis() / 1000) - 120L < result.getLong("latest_activity")) {
+                count++;
             }
         }
-        jda.getPresence().setActivity(Activity.playing("ActivePlayers: " + activeCount));
+
+        jda.getPresence().setActivity(Activity.playing("ActivePlayers: " + count));
     }
 }
