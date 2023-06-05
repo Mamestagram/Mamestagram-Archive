5b7511a0d337e1e63506e434bb1043930a0a01ae
mames1dev <kamakuraowner@outlook.jp>
2023-06-05 22:18:21 +0900

    ロールの付与機能追加

--- src/main/java/net/mamestagram/Server/RoleDistribution.java
+++ src/main/java/net/mamestagram/Server/RoleDistribution.java
@@ -12,17 +12,39 @@ public class RoleDistribution extends ListenerAdapter {
 
     private static final long CHANNELID = 1012691799350988810L;
     private static final long INFOROLEID = 1083976804680863744L;
+    private static final long OSUCATCHID = 1115259320012111913L;
+    private static final long OSUTAIKOID = 1115259058816041101L;
+    private static final long OSUMANIAID = 1115258910123773992L;
+    private static final long OSUSTDID = 1115258608557494333L;
+    private static final long MULTIID = 1115258608557494333L;
 
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent e) {
 
         Guild guild = e.getGuild();
         Member member = e.getMember();
-        Role role = guild.getRoleById(INFOROLEID);
+        Role infoRole = guild.getRoleById(INFOROLEID);
+        Role catchRole = guild.getRoleById(OSUCATCHID);
+        Role taikoRole = guild.getRoleById(OSUTAIKOID);
+        Role maniaRole = guild.getRoleById(OSUMANIAID);
+        Role stdRole = guild.getRoleById(OSUSTDID);
+        Role mutiRole = guild.getRoleById(MULTIID);
 
-        if(e.getChannel().getIdLong() == CHANNELID && e.getReaction().getEmoji().equals(Emoji.fromUnicode("U+2705"))) {
+        if(e.getChannel().getIdLong() == CHANNELID) {
 
-            guild.addRoleToMember(member, role).queue();
+            if(e.getReaction().getEmoji().equals(Emoji.fromUnicode("U+2705"))){
+                guild.addRoleToMember(member, infoRole).queue();
+            } else if(e.getReaction().getEmoji().equals(Emoji.fromFormatted("<:osu:1100702517119168562>"))) {
+                guild.addRoleToMember(member, stdRole).queue();
+            } else if(e.getReaction().getEmoji().equals(Emoji.fromFormatted("<:catch:992621083985457202>"))) {
+                guild.addRoleToMember(member, catchRole).queue();
+            } else if(e.getReaction().getEmoji().equals(Emoji.fromFormatted("<:mania:1100702514501910630>"))) {
+                guild.addRoleToMember(member, maniaRole).queue();
+            } else if(e.getReaction().getEmoji().equals(Emoji.fromFormatted("<:taiko:1100702510152429588>"))) {
+                guild.addRoleToMember(member, taikoRole).queue();
+            } else if(e.getReaction().getEmoji().equals(Emoji.fromUnicode("U+1F525"))) {
+                guild.addRoleToMember(member, mutiRole).queue();
+            }
         }
     }
 }
